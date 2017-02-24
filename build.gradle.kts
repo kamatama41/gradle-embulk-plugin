6cd532ac4947f5f322e03e9c8843e0e99b414b99
Shinichi Ishimura <shiketaudonko41@gmail.com>
2017-02-25 02:45:49 +0900

    gradle init and set gradle-script-kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+buildscript {
+    val kotlinVersion = "1.0.6"
+    extra["kotlinVersion"] = kotlinVersion
+    repositories {
+        jcenter()
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+    }
+}
+
+apply {
+    plugin("idea")
+    plugin("kotlin")
+}
+
+repositories {
+    jcenter()
+}
+
+val kotlinVersion: String by extra
+dependencies {
+    compile("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
+    testCompile("junit:junit:4.12")
+}
