aeff926d039455214f111e12990a45a318c78b3e
Shinichi Ishimura <shiketaudonko41@gmail.com>
2017-02-25 19:50:02 +0900

    Add a task and test

--- src/main/kotlin/com/github/kamatama41/EmbulkPlugin.kt
+++ src/main/kotlin/com/github/kamatama41/EmbulkPlugin.kt
@@ -1,9 +1,13 @@
 package com.github.kamatama41
 
+import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class EmbulkPlugin : Plugin<Project> {
     override fun apply(project: Project) {
+        project.tasks.create("embulk", EmbulkTask::class.java)
     }
+
+    open class EmbulkTask: DefaultTask()
 }
---
+++ src/test/kotlin/com/github/kamatama41/EmbulkPluginTest.kt
@@ -0,0 +1,16 @@
+package com.github.kamatama41
+
+import org.gradle.api.DefaultTask
+import org.gradle.testfixtures.ProjectBuilder
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class EmbulkPluginTest {
+    @Test fun dummyTest() {
+        val project = ProjectBuilder.builder().build()
+        project.pluginManager.apply("com.github.kamatama41.embulk")
+
+        val embulk = project.tasks.findByName("embulk") as DefaultTask
+        assertTrue("embulk is a EmbulkPlugin.EmbulkTask", embulk is EmbulkPlugin.EmbulkTask)
+    }
+}
