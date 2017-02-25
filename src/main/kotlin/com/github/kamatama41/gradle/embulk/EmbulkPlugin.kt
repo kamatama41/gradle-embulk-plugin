281e23bf072b5a4826b0ef6bd402779a4ec975aa
Shinichi Ishimura <shiketaudonko41@gmail.com>
2017-02-26 03:54:52 +0900

    Add classpath task

--- src/main/kotlin/com/github/kamatama41/gradle/embulk/EmbulkPlugin.kt
+++ src/main/kotlin/com/github/kamatama41/gradle/embulk/EmbulkPlugin.kt
@@ -1,13 +1,39 @@
 package com.github.kamatama41.gradle.embulk
 
-import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.plugins.JavaPlugin
+import org.gradle.api.tasks.Copy
+import org.gradle.api.tasks.bundling.Jar
 
 class EmbulkPlugin : Plugin<Project> {
     override fun apply(project: Project) {
-        project.tasks.create("embulk", EmbulkTask::class.java)
+        project.plugins.apply(JavaPlugin::class.java)
+        project.configurations.maybeCreate("provided")
+
+        classpathTask(project)
     }
 
-    open class EmbulkTask: DefaultTask()
+    fun classpathTask(project: Project) {
+        project.tasks.create("classpath", Copy::class.java) { task ->
+            project.afterEvaluate {
+                val jar = project.tasks.findByName("jar") as Jar
+                val target = "classpath"
+                val runtime = project.configurations.findByName("runtime")
+                val provided = project.configurations.findByName("provided")
+
+                task.dependsOn(jar)
+                task.group = GROUP_NAME
+
+                task.doFirst { project.file(target).deleteRecursively() }
+
+                task.from(runtime - provided + project.files(jar.archivePath))
+                task.into(target)
+            }
+        }
+    }
+
+    companion object {
+        val GROUP_NAME = "embulk"
+    }
 }
--- src/test/kotlin/com/github/kamatama41/gradle/embulk/EmbulkPluginTest.kt
+++ src/test/kotlin/com/github/kamatama41/gradle/embulk/EmbulkPluginTest.kt
@@ -1,16 +1,20 @@
 package com.github.kamatama41.gradle.embulk
 
-import org.gradle.api.DefaultTask
+import org.gradle.api.Project
+import org.gradle.api.tasks.Copy
 import org.gradle.testfixtures.ProjectBuilder
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class EmbulkPluginTest {
-    @Test fun dummyTest() {
+    val project: Project by lazy {
         val project = ProjectBuilder.builder().build()
         project.pluginManager.apply("com.github.kamatama41.embulk")
+        project
+    }
 
-        val embulk = project.tasks.findByName("embulk") as DefaultTask
-        assertTrue("embulk is a EmbulkPlugin.EmbulkTask", embulk is EmbulkPlugin.EmbulkTask)
+    @Test fun classpath() {
+        val classpath = project.tasks.findByName("classpath")
+        assertTrue("classpath is a Copy task", classpath is Copy)
     }
 }
