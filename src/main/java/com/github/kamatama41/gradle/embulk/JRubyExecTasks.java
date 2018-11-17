0bea27a110975c38810dfd2a59770bad5c137582
Shinichi Ishimura <ishimura@treasure-data.com>
2018-11-18 08:13:16 +0900

    Use Lambda instead of anonymous class

--- src/main/java/com/github/kamatama41/gradle/embulk/JRubyExecTasks.java
+++ src/main/java/com/github/kamatama41/gradle/embulk/JRubyExecTasks.java
@@ -1,9 +1,7 @@
 package com.github.kamatama41.gradle.embulk;
 
 import com.github.jrubygradle.JRubyExec;
-import org.gradle.api.Action;
 import org.gradle.api.Project;
-import org.gradle.api.Task;
 
 import java.io.File;
 
@@ -17,16 +15,13 @@ public class JRubyExecTasks {
         task.dependsOn("gem");
         task.setGroup(EmbulkPlugin.getGroupName());
 
-        project.afterEvaluate(new Action<Project>() {
-            @Override
-            public void execute(Project project) {
-                task.setJrubyVersion(extension.getJrubyVersion());
-                task.jrubyArgs("-S");
-                task.script("gem");
-                task.scriptArgs("push", project.file(String.format("pkg/%s-%s.gem", project.getName(), project.getVersion())));
+        project.afterEvaluate(evaluatedProject -> {
+            task.setJrubyVersion(extension.getJrubyVersion());
+            task.jrubyArgs("-S");
+            task.script("gem");
+            task.scriptArgs("push", evaluatedProject.file(String.format("pkg/%s-%s.gem", evaluatedProject.getName(), evaluatedProject.getVersion())));
 
-                JRubyExec.updateJRubyDependencies(project);
-            }
+            JRubyExec.updateJRubyDependencies(evaluatedProject);
         });
     }
 
@@ -35,32 +30,26 @@ public class JRubyExecTasks {
         task.dependsOn("gemspec", "classpath");
         task.setGroup(EmbulkPlugin.getGroupName());
 
-        project.afterEvaluate(new Action<Project>() {
-            @Override
-            public void execute(final Project project) {
-                task.setJrubyVersion(extension.getJrubyVersion());
-                task.jrubyArgs("-S");
-                task.script("gem");
-                task.scriptArgs("build", project.file(project.getName() + ".gemspec"));
+        project.afterEvaluate(evaluatedProject -> {
+            task.setJrubyVersion(extension.getJrubyVersion());
+            task.jrubyArgs("-S");
+            task.script("gem");
+            task.scriptArgs("build", evaluatedProject.file(evaluatedProject.getName() + ".gemspec"));
 
-                task.doLast(new Action<Task>() {
-                    @Override
-                    public void execute(Task task) {
-                        String gemFile = String.format("%s-%s.gem", project.getName(), project.getVersion());
-                        File dst = project.file("pkg/" + gemFile);
-                        if(!dst.getParentFile().exists()) {
-                            if(!dst.getParentFile().mkdirs()) {
-                                throw new IllegalStateException("Prent dir was not made.");
-                            }
-                        }
-                        if(!project.file(gemFile).renameTo(project.file("pkg/" + gemFile))) {
-                            throw new IllegalStateException("File was not renamed.");
-                        }
+            task.doLast(ignored -> {
+                String gemFile = String.format("%s-%s.gem", evaluatedProject.getName(), evaluatedProject.getVersion());
+                File dst = evaluatedProject.file("pkg/" + gemFile);
+                if(!dst.getParentFile().exists()) {
+                    if(!dst.getParentFile().mkdirs()) {
+                        throw new IllegalStateException("Prent dir was not made.");
                     }
-                });
+                }
+                if(!evaluatedProject.file(gemFile).renameTo(evaluatedProject.file("pkg/" + gemFile))) {
+                    throw new IllegalStateException("File was not renamed.");
+                }
+            });
 
-                JRubyExec.updateJRubyDependencies(project);
-            }
+            JRubyExec.updateJRubyDependencies(evaluatedProject);
         });
     }
 }
